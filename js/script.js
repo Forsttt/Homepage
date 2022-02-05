0e4987454f8936423888c908900ca11f14130e34
Szymon Forst <szymonforstbiznes@gmail.com>
2022-02-05 17:04:58 +0100

    changed class name

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ console.log("Witam");
 
 let button = document.querySelector(".js-button");
 let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName")
+let themeName = document.querySelector(".js-themeName");
 
 button.addEventListener("click", () => {
-    body.classList.toggle("blue");
-    themeName.innerText = body.classList.contains("blue") ? "jasny niebieski" : "jasny różowy";
+    body.classList.toggle("colorChange");
+    themeName.innerText = body.classList.contains("colorChange") ? "jasny niebieski" : "jasny różowy";
 });
 
 
